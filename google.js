382847fba64b131f4b8e65bd20fc7cb119591913
ypresion <sylwia.krupa@yahoo.com>
2020-03-17 21:53:04 +0000

    scraped data needed from google play store

--- google.js
+++ google.js
@@ -1,18 +1,18 @@
 var scraper = require('google-play-scraper');
 var fs = require('fs');
 
-var logStream = fs.createWriteStream('gamble.json', {flags: 'a'});
+var logStream = fs.createWriteStream('casino_Google.json', {flags: 'a'});
 
 scraper.search({
-    term: "gamble",
-    num: 2,
+    term: "casino",
+    num: 250,
     country: "gb",
     price: "free",
     throttle: 10,
     fullDetail: true 
 })
 .then(function(result) {
-    const newResult = result.map(({url, descriptionHTML, minInstalls, scoreText, free, currency, priceText, offersIAP, size, androidVersion, androidVersionText, developerId, developerEmail, developerWebsite, developerAddress, privacyPolicy, developerInternalID, genreId, icon, headerImage, screenshots, adSupported, comments, editorsChoice, ...details}) => details)
+    const newResult = result.map(({url, descriptionHTML, minInstalls, scoreText, free, currency, priceText, offersIAP, size, androidVersion, androidVersionText, developerId, developerEmail, developerWebsite, developerAddress, privacyPolicy, developerInternalID, genreId, icon, headerImage, screenshots, adSupported, comments, editorsChoice, histogram, video, videoImage, ...details}) => details)
     saveResult(newResult);
 })
 .catch(console.log);
