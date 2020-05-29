4b3df23d71a7fcd27b6beff396f1b1a188e7de73
ypresion <sylwia.krupa@yahoo.com>
2020-05-29 07:21:34 +0100

    simplified the main function

--- scripts/parser.js
+++ scripts/parser.js
@@ -3,38 +3,27 @@ const { Parser } = require('json2csv');
 const xls = require('json2xls');
 
 //Parse JSON to csv
-//@param file file name
-//@param platform google or apple
-//@date date string in DD.MM format
+//@param save path to save file
+//@param read path of the file to parse
 //@format csv or xls
-function parseData(file, platform, date, format) {
-    let path;
-    switch(platform) {
-        case 'apple':
-            path = '../appleData/' + date + '/' + file + '.json';
-            break;
-            
-        case 'google':
-            path = '../googleData/' + date + '/' + file + '.json';
-            break;
-        }
+function parseData(save, read, format) {
 
-    let rawData = fs.readFileSync(path);
+    let rawData = fs.readFileSync(read);
     let data = JSON.parse(rawData);
             
     switch(format) {
         case 'csv':
             const parser = new Parser();
             let csv = parser.parse(data);
-            fs.writeFileSync('../' + file + '.csv', csv);
+            fs.writeFileSync(save, csv);
         break;
 
         case 'xls':
             let xlsx = xls(data);
-            fs.writeFileSync(file + '.xlsx', xlsx, 'binary');   
+            fs.writeFileSync(save, xlsx, 'binary');   
         break;     
     }              
 }
 
 //example
-parseData('all', 'apple', '12.05', 'csv');
+parseData('../csv/slots.csv', '../appleData/12.05/slots.json', 'csv');
