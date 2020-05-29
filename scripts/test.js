0c6195d371c9431755c48a7709c72986b073e98a
ypresion <sylwia.krupa@yahoo.com>
2020-05-29 07:30:25 +0100

    delete test file

--- scripts/test.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-const json2xls = require('json2xls');
-
-let rawData = fs.readFileSync('../googleStoreData/24.03/bet.json');
-let data = JSON.parse(rawData);
-
-let xls = json2xls(data);
-fs.writeFileSync('data.xlsx', xls, 'binary');
-
